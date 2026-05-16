65125c33d152f12803aa048fa2b1551989e33b18
pawangang0329 <bermudezpaola42@gmail.com>
2026-05-15 21:24:46 -0500

    url y key de la base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 
-  supabaseUrl: '',
-  supabaseKey: ''
+  supabaseUrl: 'https://supabase.com/dashboard/project/xpalyvdmhjylnthxfdjp/sql/03786a9e-d252-4f5a-8725-1613f3f35359',
+  supabaseKey: 'sb_publishable_LUwCvWGYqQkD3uaGTKNuAA_h2B6Kca-'
 };
